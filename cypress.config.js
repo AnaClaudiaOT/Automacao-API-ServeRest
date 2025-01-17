b4341b339c1713c1aab68013f7ace122564c01e1
AnaClaudiaOT <ana.claudia.tavares@hotmail.com>
2025-01-17 16:08:51 -0300

    Configuração de CI e Relatório Mocha

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,19 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require("cypress")
 
 module.exports = defineConfig({
   reporter: "cypress-mochawesome-reporter",
   reporterOptions: {
-    reportDir: "cypress/reports",
-    overwrite: false,
-    html: true,
-    json: true,
+    reportDir: "cypress/reports",  // Direciona os arquivos de relatório para esse diretório
+    overwrite: true,               // Substitui os arquivos existentes
+    html: true,                    // Gera o arquivo HTML
+    json: true,                    // Gera o arquivo JSON
+    mochaFile: "cypress/reports/results-[hash].json", // Arquivo JSON único para cada execução
   },
   e2e: {
     baseUrl: "https://serverest.dev",
     setupNodeEvents(on, config) {
-      require("cypress-mochawesome-reporter/plugin")(on);
-      return config;
+      require("cypress-mochawesome-reporter/plugin")(on)
+      return config
     },
   },
-});
+})
